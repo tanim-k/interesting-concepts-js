eb84aa21cd56cd7955c422267603d7e48beab632
tanim-k <tanimkhan877@gmail.com>
2022-03-17 11:16:56 +0600

    variable value comparison

---
+++ double-triple.js
@@ -0,0 +1,20 @@
+const first = 2;
+const second = true;
+if(first === second){
+    console.log('condition is true');
+}
+else {
+    console.log('condition is false')
+}
+
+// more comparison
+// const a = {name:'ali'};
+// const b = {name:'ali'};
+const a = [];
+const b = []
+if(a === b) {
+    console.log('both are same');
+}
+else{
+    console.log('they r not same');
+}
