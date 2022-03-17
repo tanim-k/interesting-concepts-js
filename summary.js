4428fa9b76b212d026fa4074bd40f36bc7a22067
tanim-k <tanimkhan877@gmail.com>
2022-03-17 16:09:57 +0600

    call back

---
+++ summary.js
@@ -0,0 +1,26 @@
+const a = 'alim khan'
+const b = 44;
+const c = true;
+/* null; undefined primitive objective object  */
+const num = [23,44,55,66,54];
+console.log(typeof num);
+console.log(Array.isArray(num))
+
+function triple(x, y, z) {
+    x.age = 888
+    // y=222
+    // z=333
+
+}
+const num1 = 3;
+const num2 = 5;
+const num3 = 7;
+const myObj = {name:'kuddus', age: 17};
+triple(myObj)
+console.log(myObj)
+// triple(num1, num2, num3)
+// console.log(num1, num2, num3)
+// console.log(triple.length)
+// console.log(typeof triple)
+// console.log(typeof triple.length)
+// console.log(typeof null)
